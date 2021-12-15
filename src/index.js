bbf3b5710400f40368b71bfe45598e208dde689b
aoehiagwina <a.o.ehiagwina@gmail.com>
2021-12-15 03:48:05 +0000

    using file system

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-
-const movie = require("./util");
+//import the utilities value from util/index.js
+const {Movie, pullMovieData} = require("./util");
+//import yargs from yargs
 const yargs = require("yargs");
-// const { command } = require("yargs");
+//assign yargs to argv
 const args = yargs.argv;
 
 
@@ -9,18 +10,24 @@ const App = () => {
     switch (args.command) {
         case "add":
             console.log("Working");
-            const newMovie = new movie (args.title, args.actor);
+            const newMovie = new movie (args.title, args.actors);
             newMovie.add();
+            newMovie.list();
+            newMovie.save();
             break;
         case "add_more":
-            const newMovie1 = new movie (args.title1, args.actor1);
-            const newMMovie2 = new movie (args.title2, args.actor2);
+            const newMovie1 = new movie (args.title1, args.actors1);
+            const newMMovie2 = new movie (args.title2, args.actors2);
             newMovie1.add();
             newMMovie2.add();
+            newMovie1.save();
+            newMovie2.save();
+            newMovie1.list();
     
-        default:console.log ("Not Found")
+        default:
+            console.log ("Not Found")
             break;
     }
-}
+};
 
 App();
--- src/util/index.js
+++ src/util/index.js
@@ -1,16 +1,32 @@
+const fs = require("fs");
+let MovieArray;
 
-const MovieArray = [];
+const pullMovieData = () => {
+    try{
+        //convert json into js object from predefirned storage location
+        MovieArray = JSON.parse(fs.readFileSync("./storage.json"));
+    } catch (error) {
+        MovieArray = [];
+    }
+};
+
 
 class Movie {
-    constructor(title, actor = "Not Specified"){
+    constructor(title, actors = "Not Specified"){
         this.title = title;
-        this.actor = actor;
+        this.actors = actors;
     }
     
     add() {
         MovieArray.push(this);
+    }
+    list() {
         console.log(MovieArray);
+    }
+    save() {
+        //This code coverts and save js as json in the define storage location 
+        fs.writeFileSync("./storage.json", JSON.stringify)
     }
 }
 
-module.exports = Movie;
+module.exports = {Movie, pullMovieData};
